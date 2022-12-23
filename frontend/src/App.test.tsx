5657edf43b1e972c75f158a22a024548c88d3d79
Cory Tomlinson <corytomlinson6640@gmail.com>
2022-12-23 07:00:57 -0700

    remove starter code

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  expect(screen.getByText(/learn react/i)).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   expect(screen.getByText(/learn react/i)).toBeInTheDocument();
+// });
+
+test('placeholder test', () => expect(true).toBe(true));
